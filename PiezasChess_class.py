8c9d2af00ab94f1673fa2d57d7a539ea311ea9b0
Arabella Blandin <143655544+axbland@users.noreply.github.com>
2024-06-17 16:37:12 -0600

    Update Main

--- PiezasChess_class.py
+++ PiezasChess_class.py
@@ -14,28 +14,33 @@ class Piezas:
 
 class Peon(Piezas):
     def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super.__init__(nombre, color, movimiento, cantidad, captura)
+        super().__init__(nombre, color, movimiento, cantidad, captura)
     
     def promo(self):
-        return f"El {self.nombre} {self.color} se puede promocionar a un alfil, torre o dama"
+        return f"El {self.nombre} {self.color} se puede promocionar a un alfil, torre o dama"     
         
+#main
+def Main():
+    reyB = Piezas("Rey", "Blanco", "Lineal y Diagonal", 1, "Lineal y Diagonal")
+    reyN = Piezas("Rey", "Negro", "Lineal y Diagonal", 1, "Lineal y Diagonal")
+    print(reyN.mover())
+    print(reyB.capturar())
     
-class Torre(Piezas):
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super.__init__(nombre, color, movimiento, cantidad, captura)
+    alfilB = Piezas("Alfil", "Blanco", "Diagonal", 2, "Diagonal")
+    alfilN = Piezas("Alfil", "Negro", "Diagonal", 2, "Diagonal")
     
-class Alfil(Piezas):
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super.__init__(nombre, color, movimiento, cantidad, captura)
+    torreB = Piezas("Torre", "Blanco", "Lineal", 2, "Lineal")
+    torreN = Piezas("Torre", "Negro", "Lineal", 2, "Lineal")
     
-class Rey(Piezas):
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super.__init__(nombre, color, movimiento, cantidad, captura)
+    peonB = Peon("Peon", "Blanco", "Lineal", 8, "Lineal y Al Paso")
+    peonN = Peon("Peon", "Negro", "Lineal", 8, "Lineal y Al Paso")
+    print(peonN.promo())
+        
+    damaB = Piezas("Dama", "Blanco", "Lineal y Diagonal", 1, "Lineal y Diagonal")
+    damaN = Piezas("Dama", "Negro", "Lineal y Diagonal", 1, "Lineal y Diagonal")
     
-class Dama(Piezas):
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super.__init__(nombre, color, movimiento, cantidad, captura)
+    caballoB = Piezas("Caballo", "Blanco", "L", 1, "L")
+    caballoN = Piezas("Caballo", "Negro", "L", 1, "L")
     
-class Caballo(Piezas):
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super.__init__(nombre, color, movimiento, cantidad, captura)
+if __name__ == "__main__":
+    Main()
