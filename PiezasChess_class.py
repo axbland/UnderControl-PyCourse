73c0cb069b7297b74a6b287e14dc446fd6f41863
Arabella Blandin <143655544+axbland@users.noreply.github.com>
2024-06-20 23:17:55 -0600

    Update PiezasChess_class.py
    Tarea mejorada, como el Señor manda xd

--- PiezasChess_class.py
+++ PiezasChess_class.py
@@ -1,5 +1,6 @@
 class Piezas:
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
+    def __init__(self, tipo, nombre, color, movimiento, cantidad, captura):
+        self.tipo = tipo
         self.nombre = nombre
         self.color = color
         self.movimiento = movimiento
@@ -7,40 +8,13 @@ class Piezas:
         self.captura = captura
     
     def mover(self):
-        return f"{self.nombre} {self.color} se puede mover de forma {self.movimiento}"
+        return f"{self.nombre} {self.color} se puede mover de forma {self.movimiento}, tiene un total de {self.cantidad} {self.tipo}"
     
     def capturar(self):
-        return f"{self.nombre} {self.color} puede capturar piezas de forma {self.movimiento}"
-
-class Peon(Piezas):
-    def __init__(self, nombre, color, movimiento, cantidad, captura):
-        super().__init__(nombre, color, movimiento, cantidad, captura)
-    
-    def promo(self):
-        return f"El {self.nombre} {self.color} se puede promocionar a un alfil, torre o dama"     
+        return f"{self.nombre} {self.color} puede capturar piezas de forma {self.movimiento}"   
         
 #main
-def Main():
-    reyB = Piezas("Rey", "Blanco", "Lineal y Diagonal", 1, "Lineal y Diagonal")
-    reyN = Piezas("Rey", "Negro", "Lineal y Diagonal", 1, "Lineal y Diagonal")
-    print(reyN.mover())
-    print(reyB.capturar())
-    
-    alfilB = Piezas("Alfil", "Blanco", "Diagonal", 2, "Diagonal")
-    alfilN = Piezas("Alfil", "Negro", "Diagonal", 2, "Diagonal")
-    
-    torreB = Piezas("Torre", "Blanco", "Lineal", 2, "Lineal")
-    torreN = Piezas("Torre", "Negro", "Lineal", 2, "Lineal")
-    
-    peonB = Peon("Peon", "Blanco", "Lineal", 8, "Lineal y Al Paso")
-    peonN = Peon("Peon", "Negro", "Lineal", 8, "Lineal y Al Paso")
-    print(peonN.promo())
-        
-    damaB = Piezas("Dama", "Blanco", "Lineal y Diagonal", 1, "Lineal y Diagonal")
-    damaN = Piezas("Dama", "Negro", "Lineal y Diagonal", 1, "Lineal y Diagonal")
-    
-    caballoB = Piezas("Caballo", "Blanco", "L", 1, "L")
-    caballoN = Piezas("Caballo", "Negro", "L", 1, "L")
-    
-if __name__ == "__main__":
-    Main()
+#def Main():
+
+#if __name__ == "__main__":
+    #Main()
