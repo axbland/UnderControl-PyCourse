9c3759587baacc4fb2f8c3d70612270b56952bcc
Arabella Blandin <143655544+axbland@users.noreply.github.com>
2024-07-08 14:07:56 -0600

    Update tareaPiezas

--- Torres.py
+++ Torres.py
@@ -1,5 +1,11 @@
 from PiezasChess_class import Piezas
+from Tablero import Tablero 
 
 class Torre(Piezas):
-    def __init__(self, tipo, nombre, color, movimiento, cantidad, captura):
+    def __init__(self, tipo, nombre, color, movimiento, cantidad, captura, fila_actual, columna_actual):
         super().__init__(tipo, nombre, color, movimiento, cantidad, captura)
+        self.fila_actual = fila_actual
+        self.columna_actual = columna_actual
+        
+def positionT(self):
+        print(f"La torre {self.color} se encuentra en la fila {self.fila_actual}, columna {self.columna_actual}.")
