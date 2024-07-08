6bfd516d872588ba2a00227c01a562b72bc6258f
Arabella Blandin <143655544+axbland@users.noreply.github.com>
2024-07-08 14:07:24 -0600

    Update tareaPiezas

--- PiezasChess_class.py
+++ PiezasChess_class.py
@@ -1,20 +1,29 @@
 class Piezas:
-    def __init__(self, tipo, nombre, color, movimiento, cantidad, captura):
+    def __init__(self, tipo, nombre, color, movimiento, cantidad, captura): #posicion
         self.tipo = tipo
         self.nombre = nombre
         self.color = color
         self.movimiento = movimiento
         self.cantidad = cantidad
         self.captura = captura
+        #self.posicion[letra][numero] = posicion
     
-    def mover(self):
+    def forma_mover(self):
         return f"{self.nombre} {self.color} se puede mover de forma {self.movimiento}, tiene un total de {self.cantidad} {self.tipo}"
     
     def capturar(self):
         return f"{self.nombre} {self.color} puede capturar piezas de forma {self.movimiento}"   
-        
+
+    def movement(self):
+        print(f"La pieza {self.nombre} {self.color} ahora está en la posición {self.letra}{self.numero}")  
+        self.letra = input("Ingrese el movimimento (Columna/Letra): ")
+        self.numero = input("Ingrese el movimimento (Fila/Numero): ")
+        return f"La pieza {self.nombre} {self.color} ahora está en la posición {self.letra}{self.numero}"
+
+
 #main
 #def Main():
 
 #if __name__ == "__main__":
     #Main()
+
